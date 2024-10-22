d8c863fd0cbd1b67f46a63fa6040bf4d1f0ab906
uwua <rocacruzaxel48@gmail.com>
2024-10-22 16:39:47 -0500

    1463B

--- Problems/1918C.cpp
+++ Problems/1918C.cpp
@@ -16,6 +16,7 @@ using namespace std;
 #define fer(i, a, b) for(ll i = (ll)a ; i >= (ll)b; i--)
 
 void so(int test){
+    cout << " test "<< test << ":\n";
     ll a,b,r;
     cin >> a >> b >> r;
     ll mas = (a^b);
@@ -37,20 +38,56 @@ void so(int test){
         cout << abs((a^0) - (b^0)) << ln;
         return;
     }
+
+    int pos = -1;
+    for(int i = 0 ; i < 63 ; i++) if ((mas>>i)&1) pos = i;
+
     ll ans = 1e18;
-    ll x = 0;
-    // case a mayor -> a cede todos sus 1 (bits prendidos), sin que pierda la caracteristica de ser mayor
-    fer(i,point,0)if((mas_a>>i)&1 and x + (1<<i) <= r ){
-        x += (1<<i) ;
+    if((a>>(point+1)) == (b>>(point+1))) {
+        ll x =0 ;
+        int cda = ( 1&(mas_a >> point) > 1&(mas_b >>point)? 1 :0);
+        if(cda == 0) x = (1ll<<point);
+        fer(i, point-1,0) if( (mas_a>>i)&1 and x + (1<<i) <= r){
+            x += (1<<i);
+        }
+        ans = min(ans, abs((a^x)-(b^x) ));
+        cout << (a^x)-(b^x) << ln;
+        x = 0;
+        if(cda == 1) x = (1ll<<point);
+        fer(i, point-1,0) if( (mas_b>>i)&1 and x + (1<<i) <= r){
+            x += (1<<i);
+        }
+
+        ans = min(ans, abs((a^x)-(b^x) ));
+        cout << (a>>(point+1)) << " " << (b>>(point+1)) << ln;
+        fu(a);
+        fu(mas);
+        fu( (1ll<<(point)) );
+        fu(b);
+        cout << ans;
+        cout << "===================\n";
+
+    }else if((a>>(point+1)) < (b>>(point+1))){
+        ll x = 0;
+        // case : b mayor -> b cede todos sus 1 (bits prendidos)
+        fer(i,point,0)if((mas_b>>i)&1 and x + (1<<i) <= r ){
+            x += (1<<i) ;
+        }
+        ans = min(ans, abs((a^x)-(b^x) ));
+        cout << ans << ln;
+        cout << "===================\n";
+    }else {
+        ll x = 0;
+        // case a mayor -> a cede todos sus 1 (bits prendidos), sin que pierda la caracteristica de ser mayor
+        fer(i,point,0)if((mas_a>>i)&1 and x + (1<<i) <= r ){
+            x += (1<<i) ;
+        }
+        ans = min(ans, abs((a^x)-(b^x) ));
+        cout << ans << ln;
+        cout << "===================\n";
     }
-    ans = min(ans, abs((a^x)-(b^x) ));
-    x = 0;
-    // case : b mayor -> b cede todos sus 1 (bits prendidos)
-    fer(i,point,0)if((mas_b>>i)&1 and x + (1<<i) <= r ){
-        x += (1<<i) ;
-    }
-    ans = min(ans, abs((a^x)-(b^x) ));
-    cout << ans << ln; }
+}
+
 
 
 int main() {
