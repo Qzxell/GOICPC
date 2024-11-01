46df0e9cc701551e8583c19f02459b7f15236f3a
Axl <rocacruzaxel48@gmail.com>
2024-11-01 16:03:28 -0500

    asdf

---
+++ Problems/1644C.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define sz(v) ((int)(v).size())
+#define all(v) (v).begin(),(v).end()
+#define ln '\n'
+#define ll long long
+#define pb push_back
+#define ii pair<int,int>
+#define vii vector<ii>  
+#define vi vector<int>  
+#define fi first
+#define se second
+#define f(i, a, b) for(ll i = (ll)a; i < (ll)b; i++)
+#define fer(i, b, a) for(ll i = (ll)a - 1; i >= (ll)b; i--)
+
+int fu(int l, int r){
+    if (l > r ) {
+        return 0; 
+    }
+
+}
+void so(int test){
+    int n,k;
+    cin >> n >> k;
+    vi v(n);
+
+    f(i,0,n)cin >> v[i];
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tt = 1;
+    cin >> tt;
+    int test = 1;
+    while (tt--){
+        so(test++);
+    }
+    return 0;
+}
