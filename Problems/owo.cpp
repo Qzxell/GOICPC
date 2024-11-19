559eaf6684b26b0a4a7eae134d8bcc60146e9f07
uwua <rocacruzaxel48@gmail.com>
2024-11-19 15:58:12 -0500

    owo

--- Problems/owo.cpp
+++ Problems/owo.cpp
@@ -1,3 +1,4 @@
+#pragma GCC optimize ("Ofast,unroll-loops")
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
@@ -16,7 +17,6 @@ typedef vector<ii > vii;
 #define all(v) (v).begin(), (v).end()
 #define sz() size()
 #define trace(...) fff(#__VA_ARGS__, __VA_ARGS__)
-#define ln '\n'
 template<typename t> void fff(const char* x, t&& val1) { cout<<x<< " : "<<val1<<"\n";}
 template<typename t1, typename... t2> void fff(const char* x, t1&& val1, t2&&... val2){
     const char* xd=strchr(x+1, ',');
@@ -25,7 +25,7 @@ template<typename t1, typename... t2> void fff(const char* x, t1&& val1, t2&&...
 }
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-const ll mod = 1e9 + 7, inf = (1 << 30) - 1 + (1 << 30);
+const ll mod = 998244353, inf = (1 << 30) - 1 + (1 << 30);
 const ll infl = (1LL << 62) - 1 +(1LL << 62);
 ll add(ll a, ll b){
     if(a + b >= mod){
@@ -42,65 +42,33 @@ ll rem(ll a, ll b){
 ll mul(ll a, ll b){
     return (ll) (a * b) % mod;
 }
-const ll N = 5e5 + 5;
-int A[N], ma[N], ans[N], n;
-vi adj[N];
-bool vist[N];
-void dfs(int a){
-    vist[a] = 1;
-    ans[a] = ma[a];
-    for(auto v: adj[a]){
-        if(!vist[v]) dfs(v);
-        ans[a] = max(ans[a], ans[v]);
-    }
-}
+const ll N = 1e6 + 5;
+int A[N], ans[N], u[N];
+vi d[N];
 int main(){
     fastio;
-    int t; cin >> t; 
-    int cont = 1;
-    while(t--){ 
-        cin >> n;
-        cout << "test " << cont++ << ln;
-        vii aux;
-        f(i, 1, n + 1) cin >> A[i], aux.pb({A[i], i});
-        sort(all(aux),[&](auto a, auto b){
-                if(a.ff == b.ff) return a.ss < b.ss;
-                return a.ff < b.ff;
-                });
-        int cur = 0;
-        for(auto [val, in]: aux){
-            cur = max(cur, in);
-            adj[in].pb(cur);
+    f(i, 1, N) u[i] = -1; // MOwbius
+    f(i, 2, N){
+        if(!d[i].sz()){
+            for(int j = i; j < N; j += i) u[j] *= -1;
+            ll c = i*i;
+            for(ll j = c; j < N; j += c) u[j] = 0;
         }
-        for(int i = 0 ; i < n ; i++){
-            if (adj[i].empty()) {
-                continue; 
-            }
-            cout << "P " << i <<":" << ln;
-            for(int k : adj[i]){
-                cout << k << ' ';
-            }
-            cout << ln;
-        }
-
-        int in = 1;
-        f(i, 1, n + 1){
-            if(A[i] > ma[i - 1]){
-                ma[i] = A[i];
-                in = i;
-            }else ma[i] = ma[i - 1];
-            adj[i].pb(in);
-        }
-        f(i, 1, n + 1){
-            if(!vist[i]) dfs(i);
-            cout << ans[i] <<' ';
-        }
-        cout <<'\n';
-        f(i, 1, n + 1){
-            vist[i] = 0, ans[i] = 0;
-            vi ().swap(adj[i]);
-        }
+        for(int j = i; j < N; j += i) d[j].pb(i);
     }
+
+    int n; cin >> n;
+    f(i, 0, n) cin >> A[i];
+
+    for(auto x: d[A[n - 1]]) ans[x] = 1;
+    fer(i, 1, n - 1){
+        int cont = 0;
+        for(auto x: d[A[i]]) cont = add(cont, rem(u[x]*ans[x], 0));
+        for(auto x: d[A[i]]) ans[x] = add(ans[x], cont);
+    }
+    int an = 0;
+    for(auto x: d[A[0]]) an = add(an, rem(u[x]* ans[x], 0));
+    cout << an <<'\n';
     return 0;
 }
 
