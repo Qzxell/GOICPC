dca909aa7a2788575f1ba2b476ca28ca8cc2dd7c
uwua <rocacruzaxel48@gmail.com>
2025-03-27 08:24:36 -0500

    recall

---
+++ Problems/2026C.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ii = pair<int,int>;
+using vii = vector<ii>  ;
+using vi = vector<int>  ;
+#define ln  '\n'
+#define ll long long
+#define pb push_back
+#define fi first
+#define se second
+#define sz(v) ((int)(v).size())
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define f(i, a, b)  for(ll i = (ll)a; i < (ll)b; i++)
+#define fer(i, b, a)  for(ll i = (ll)a - 1; i >= (ll)b; i--)
+
+void so(int test){
+	int n;
+	cin >> n;
+	string s;
+	cin >> s;
+	vi ind;
+	f(i,0,n)if(s[i] == '1')ind.pb(i+1);
+	int le = sz(ind);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int tt = 1;
+	cin >> tt;
+	int test = 1;
+	while (tt--){
+		so(test++);
+	}
+	return 0;
+}
---
+++ Problems/abc394e.md
@@ -0,0 +1,22 @@
+---
+tags:
+  - DFS 
+Level: 1000
+Solved: true 
+Idea_clave: cambiar de perspectiva, que es un palindromo? -> como se construye o destruye
+MIstakes: 
+resolve_again_obligatory: true
+URL: 
+---
+
+## De que va el problema
+
+- 
+
+### Posibles solucion
+
+
+## Lista de WA antes del AC
+
+## Metacognition (Existe mejores, otras soluciones? Puedes demostrar, estar completamente seguro de tu solucion, demustralo!!)
+
